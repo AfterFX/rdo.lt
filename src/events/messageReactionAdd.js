b141c527fd15c6d9fc057a764e7f23412ad82f6e
AfterFX <afterfx1995@gmail.com>
2021-09-30 22:11:55 +0200

    added role in info channel role select

--- src/events/messageReactionAdd.js
+++ src/events/messageReactionAdd.js
@@ -1,3 +1,7 @@
+function role_list (reaction, role_name) {
+    return reaction.message.guild.roles.cache.find(r => r.name === role_name);
+}
+
 module.exports = {
     name: 'messageReactionAdd',
     async execute(reaction, user) {
@@ -16,15 +20,34 @@ module.exports = {
 
         if(reaction.message.channelId === process.env.verifyChannelId){//verify channel moderating verify role
             try{
-                reaction.message.guild.members.cache.get(user.id).roles.add(this.role_list(reaction, process.env.verifyRoleName).id).then( () => {
+                reaction.message.guild.members.cache.get(user.id).roles.add(role_list(reaction, process.env.verifyRoleName).id).then( () => {
                     console.log(`User: ${user} have has ✅verified on server!`);
                 })
             } catch (error) {
                 console.error('Something went wrong when adding verify role:', error);
             }
+        }else if(reaction.message.channelId === process.env.infoChannelId) {
+            //If found already selected game platform, will remove reaction. Blocks multiple reactions.
+            if (this.user_roles(reaction, user, process.env.pcRoleName) === true || this.user_roles(reaction, user, process.env.psRoleName) === true || this.user_roles(reaction, user, process.env.xboxRoleName) === true) {
+                reaction.users.remove(user);
+            } else {
+                if(reaction._emoji.name === process.env.psRoleName){
+                    reaction.message.guild.members.cache.get(user.id).roles.add(role_list(reaction, process.env.psRoleName).id)
+                    console.log(`User: ${user} have selected gaming platform: PLAYSTATION`);
+                }else if(reaction._emoji.name === process.env.xboxRoleName){
+                    reaction.message.guild.members.cache.get(user.id).roles.add(role_list(reaction, process.env.xboxRoleName).id)
+                    console.log(`User: ${user} have selected gaming platform: XBOX`);
+                }else if(reaction._emoji.name === process.env.pcRoleName){
+                    reaction.message.guild.members.cache.get(user.id).roles.add(role_list(reaction, process.env.pcRoleName).id)
+                    console.log(`User: ${user} have selected gaming platform: PC`);
+                }
+            }
+
         }
     },
-    role_list: (reaction, role) => {
-        return reaction.message.guild.roles.cache.find(r => r.name === role);
+    user_roles: (reaction, user, role_name) => {
+        const array1 = reaction.message.guild.members.cache.get(user.id).roles.member._roles;
+        const found = array1.find(element => element === role_list(reaction, role_name).id);
+        return found !== undefined;
     }
 };
--- src/events/messageReactionRemove.js
+++ src/events/messageReactionRemove.js
@@ -1,3 +1,7 @@
+function role_list (reaction, role_name) {
+    return reaction.message.guild.roles.cache.find(r => r.name === role_name);
+}
+
 module.exports = {
     name: 'messageReactionRemove',
     async execute(reaction, user) {
@@ -16,15 +20,26 @@ module.exports = {
 
         if(reaction.message.channelId === process.env.verifyChannelId){//verify channel moderating verify role
             try{
-                reaction.message.guild.members.cache.get(user.id).roles.remove(this.role_list(reaction, process.env.verifyRoleName).id).then( () => {
+                reaction.message.guild.members.cache.get(user.id).roles.remove(role_list(reaction, process.env.verifyRoleName).id).then( () => {
                     console.log(`User: ${user} have has remove ✅verified role`);
                 })
             } catch (error) {
                 console.error('Something went wrong when removing verify role:', error);
             }
+        }else if(reaction.message.channelId === process.env.infoChannelId) {
+            if(reaction._emoji.name === process.env.psRoleName){
+                this.remove_role(reaction, user, process.env.psRoleName);
+                console.log(`User: ${user} have removed gaming platform: PS4`);
+            }else if(reaction._emoji.name === process.env.xboxRoleName){
+                this.remove_role(reaction, user, process.env.xboxRoleName);
+                console.log(`User: ${user} have removed gaming platform: XBOX one`);
+            }else if(reaction._emoji.name === process.env.pcRoleName){
+                this.remove_role(reaction, user, process.env.pcRoleName);
+                console.log(`User: ${user} have removed gaming platform: PC`);
+            }
         }
     },
-    role_list: (reaction, role) => {
-        return reaction.message.guild.roles.cache.find(r => r.name === role);
+    remove_role: (reaction, user, role_name) => {
+        return reaction.message.guild.members.cache.get(user.id).roles.remove([role_list(reaction, role_name).id]);
     }
 };
