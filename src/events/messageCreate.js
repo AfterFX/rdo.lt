e2126215f95a36708488018cc924ef32a8af0a01
AfterFX <afterfx1995@gmail.com>
2021-10-06 18:25:47 +0200

    added verify interaction

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -8,7 +8,7 @@ module.exports = {
             .addComponents([this.verify_platform_getStart()]);
        if(message.content === "verify_platform") {
             message.delete();
-            client.channels.cache.get(process.env.verifyChannelId).send({  embeds: [this.verify_platform_embed()], components: [rowPlatformButtons] });
+            client.channels.cache.get(process.env.verifyChannelId).send({  embeds: [this.verify_aboutServer_embed(), this.verify_platform_embed()], components: [rowPlatformButtons] });
         }
     },
     verify_platform_getStart: () => {
@@ -18,11 +18,14 @@ module.exports = {
             .setStyle("SECONDARY")
             .setCustomId("verify_platform_getStart_button")
     },
+    verify_aboutServer_embed: () => {
+        return new MessageEmbed()
+            .setColor('#0099ff')
+            .addField(`**GENERAL RULES**`, `Lietuviška **Red Dead Online** žaidimo bendruomenė, \n napjungianti visų platformų žaidėjus.\n**Serveryje rasi:** \n<:outlaw:893402804096483368> Custom **Outlaw** bota.\n<:sheriff:777209942931013633> Serverio Staff'as visada pagelbės esant klausimams.\n<:honorable:669866015437619200> Aktyvi bendruomenė, visada rasi būrį su kuo pažaisti.\n<:clue:777209925461999627> Naudingi patarimai, pagalba ir kita.`)
+    },
     verify_platform_embed: () => {
         return new MessageEmbed()
             .setColor('#0099ff')
-            .setTitle('Some title')
-            .setURL('https://discord.js.org')
-            .setDescription('Some description here');
+            .addField(`**PLATFORMOS ROLĖS**`, `Pasirink platforma, kuria naudoji žaisdamas Red Dead Online \n žaidimą, paspausdamas ant žemiau esančio platformos mygtuko.\n\n<:verify:893065643862159390> *Tik pasirinkę platformos rolę matysitę visus serverio kanalus.*`)
     }
 };
