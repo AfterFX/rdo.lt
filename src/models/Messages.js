bf410ec0cab01037e67a95abbc1d59b2f3ef1211
AfterFX <afterfx1995@gmail.com>
2021-10-07 14:12:31 +0300

    added sequelize Messages

---
+++ src/models/Messages.js
@@ -0,0 +1,24 @@
+const { DataTypes, Model } = require('sequelize');
+
+module.exports = class Messages extends Model {
+    static init(sequelize) {
+        return super.init({
+            total:{
+                type: DataTypes.INTEGER,
+                defaultValue: 0
+            },
+            time_schedule:{
+                type: DataTypes.BIGINT,
+                defaultValue: 1631480400000
+            },
+            beforeNumber:{
+                type: DataTypes.INTEGER,
+                defaultValue: 0
+            },
+        },{
+            tableName: 'Messages',
+            timestamps: true,
+            sequelize
+        });
+    }
+}
