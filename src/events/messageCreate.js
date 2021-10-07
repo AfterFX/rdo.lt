3159bf11b51aef42f82f5a6ec2da78cd588d8a1a
AfterFX <afterfx1995@gmail.com>
2021-10-07 19:00:08 +0300

    small changes

--- src/events/messageCreate.js
+++ src/events/messageCreate.js
@@ -10,25 +10,25 @@ module.exports = {
         await this.user_total_messages(message.author);
 
         const rowPlatformButtons = new MessageActionRow()
-            .addComponents([this.verify_platform_getStart()]);
+            .addComponents([this.welcome_button()]);
        if(message.content === "_welcome") {
             message.delete();
-            client.channels.cache.get(process.env.welcomeChannelId).send({  embeds: [this.verify_aboutServer_embed(), this.verify_platform_embed()], components: [rowPlatformButtons] });
+            client.channels.cache.get(process.env.welcomeChannelId).send({  embeds: [this.welcome_1_embed(), this.welcome_2_embed()], components: [rowPlatformButtons] });
         }
     },
-    verify_platform_getStart: () => {
+    welcome_button: () => {
         return new MessageButton()
             .setLabel('Pasirinkti role')
             .setEmoji("❓")
             .setStyle("SECONDARY")
             .setCustomId("welcome_platform_getStart_button")
     },
-    verify_aboutServer_embed: () => {
+    welcome_1_embed: () => {
         return new MessageEmbed()
             .setColor('#0099ff')
             .addField(`**GENERAL RULES**`, `Lietuviška **Red Dead Online** žaidimo bendruomenė, \n napjungianti visų platformų žaidėjus.\n**Serveryje rasi:** \n<:outlaw:893402804096483368> Custom **Outlaw** bota.\n<:sheriff:777209942931013633> Serverio Staff'as visada pagelbės esant klausimams.\n<:honorable:669866015437619200> Aktyvi bendruomenė, visada rasi būrį su kuo pažaisti.\n<:clue:777209925461999627> Naudingi patarimai, pagalba ir kita.`)
     },
-    verify_platform_embed: () => {
+    welcome_2_embed: () => {
         return new MessageEmbed()
             .setColor('#0099ff')
             .addField(`**PLATFORMOS ROLĖS**`, `Pasirink platforma, kuria naudoji žaisdamas Red Dead Online \n žaidimą, paspausdamas ant žemiau esančio platformos mygtuko.\n\n<:verify:893065643862159390> *Tik pasirinkę platformos rolę matysitę visus serverio kanalus.*`)
