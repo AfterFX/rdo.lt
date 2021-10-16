50441b078cc7a5b6a35f4577fd419b5dea5e697b
AfterFX <afterfx1995@gmail.com>
2021-10-16 17:12:30 +0200

    TODO

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -5,7 +5,7 @@ module.exports = {
     name: 'interactionCreate',
     async execute(interaction, client) {
         if (interaction.isCommand()) return;
-        const buttonCommand = client.commands.get(interaction.message.interaction.commandName);//get reply motherCommand
+        const buttonCommand = client.commands.get(interaction.message.interaction.commandName);//get reply motherCommand TODO need fix, error in welcome
 
         try {
             if(!interaction.member?.permissions.has(buttonCommand.permission)){
