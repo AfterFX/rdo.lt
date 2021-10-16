6c89aa523d4013751a771a628f493157e41a87a4
AfterFX <afterfx1995@gmail.com>
2021-10-16 20:55:37 +0300

    interaction error fix

--- src/events/interactionCreate.js
+++ src/events/interactionCreate.js
@@ -5,11 +5,11 @@ module.exports = {
     name: 'interactionCreate',
     async execute(interaction, client) {
         if (interaction.isCommand()) return;
-        const buttonCommand = client.commands.get(interaction.message.interaction.commandName);//get reply motherCommand TODO need fix, error in welcome
+        // const buttonCommand = client.commands.get(interaction.message.interaction.commandName);//get reply motherCommand {buttonCommand.permission}
 
         try {
-            if(!interaction.member?.permissions.has(buttonCommand.permission)){
-                return interaction.update({embeds: [commandCatcher.no_permissions(buttonCommand)], ephemeral: true});
+            if(!interaction.member?.permissions.has("VIEW_CHANNEL")){
+                return interaction.update({embeds: [commandCatcher.no_permissions()], ephemeral: true});
             }else{
                 if(interaction.customId === "open_treasure"){
                     let button = true;
