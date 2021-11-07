d52111a81b0c22235a995b80d66643c3e35f11fa
AfterFX <afterfx1995@gmail.com>
2021-11-07 10:50:01 +0200

    small fix

--- src/database/database.js
+++ src/database/database.js
@@ -1,7 +1,7 @@
 const Sequelize = require('sequelize');//https://youtu.be/doppUudjztU?t=1642
 
 module.exports = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
-    // host: 'localhost',
+    host: '127.0.0.1',
     dialect: 'mysql',
     logging: false,
     // SQLite only
